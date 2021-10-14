68d5cd8df7101de94b9ee5a3ed5738c121767778
haze2003 <89672171+haze2003@users.noreply.github.com>
2021-10-13 21:46:33 -0400

    Create ps-lab3.ps1

---
+++ powershell/ps-lab3.ps1
@@ -0,0 +1,11 @@
+﻿#### Network Configuration Display ####
+
+## Get network adapter configuration info & assign to variable
+$netadapt=(Get-CimInstance Win32_NetworkAdapterConfiguration)
+
+## Filter only wanted information
+Write-Output $netadapt | where {$_.ipenabled -eq "True"} | 
+    Format-Table -Property "Index","Description","IPaddress", @{label="Subnet Mask"; expression={$_.ipsubnet}},
+                                                              @{label="DNS Server"; expression={$_.dnsserversearchorder}},
+                                                              @{label="DNS Domain Name"; expression={$_.dnsdomain}}
+## END OF SCRIPT
